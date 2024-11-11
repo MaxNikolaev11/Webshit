e6d1ff59c91853bdf15bdbb401e0365dcd1871ec
Max <maxnik2904@mail.ru>
2024-11-11 17:32:03 +0300

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 "use strict";
-let category = 10;
 
-category *= 3;
+const numberOfFilms = prompt("How many films have you already wached?");
 
-console.log(`https://${category}.ru`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
 
+// let movieName = prompt("One of your last seen movies?");
+// let movieRating = prompt("How you will rate it on scale from 1 to 10?");
+
+// personalMovieDB.movies[movieName] = +movieRating;
+// console.log(personalMovieDB.movies);
+
+personalMovieDB.movies[prompt("One of your last seen movies?")] = +prompt("How you will rate it on scale from 1 to 10?");
+console.log(personalMovieDB.movies);
 
